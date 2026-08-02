ade45fb8120ecc5b293e8cd8e75f83386e8b2dc2
sujalgiriiitp-source <sujalgiriiitp-source@users.noreply.github.com>
2026-08-02 21:55:30 +0530

    Add 3-new-table

---
+++ 3-new-table/17_insert_into.sql
@@ -0,0 +1,18 @@
+-- Write code below 💖
+--INSERT INTO
+-- Purpose: Table ke andar nayi row insert karna.
+
+INSERT INTO companies
+VALUES
+(1,'Google',' California', 1998);
+
+INSERT INTO companies
+VALUES
+(2,'Microsoft','Washingtom', 1975);
+
+INSERT INTO companies
+VALUES
+(3, 'Apple',' California',1976);
+
+SELECT *
+FROM companies;
