d35fe73cfdb5a0f710a7fbdf89d1c696af521322
sujalgiriiitp-source <sujalgiriiitp-source@users.noreply.github.com>
2026-08-02 19:03:50 +0530

    Add 3-new-table

---
+++ 3-new-table/create_table.sql
@@ -0,0 +1,14 @@
+-- Write code below 💖--
+CREATE TABLE companies (
+  id INTEGER,
+  name TEXT,
+  headquarters TEXT,
+  year INTEGER
+);
+
+INSERT INTO companies
+VALUES
+(1,'Goggle','California',1998);
+
+SELECT *
+FROM companies;
